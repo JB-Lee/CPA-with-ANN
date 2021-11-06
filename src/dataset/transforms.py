6b5fae9171bf24bb3fbfbbb274d62873e3620ab1
JB LEE <ttdlwhd@gmail.com>
2021-11-06 15:13:20 +0900

    :sparkles: feat: add transform

--- src/dataset/transforms.py
+++ src/dataset/transforms.py
@@ -2,11 +2,14 @@ import numpy as np
 
 
 class RandomRoll(object):
-    def __init__(self, amount):
+    def __init__(self, amount, steps):
         self.amount = amount
+        self.steps = steps
 
     def __call__(self, sample):
         trace = sample[0][:1]
 
-        r = np.random.randint(self.amount)
-        np.roll(trace, 1, axis=1)
+        r = np.random.randint(self.amount) * self.steps
+        sample[0][:1] = np.roll(trace, r, axis=1)
+
+        return sample
